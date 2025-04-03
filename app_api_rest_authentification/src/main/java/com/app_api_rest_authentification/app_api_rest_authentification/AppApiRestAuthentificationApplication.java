f21b541a8393d86ab6bf3493186c4994d7719e92
Mathéo <matheo.jeanneret@gmail.com>
2025-04-03 13:53:56 +0200

    création de l'api rest authentification

---
+++ app_api_rest_authentification/src/main/java/com/app_api_rest_authentification/app_api_rest_authentification/AppApiRestAuthentificationApplication.java
@@ -0,0 +1,13 @@
+package com.app_api_rest_authentification.app_api_rest_authentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApiRestAuthentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApiRestAuthentificationApplication.class, args);
+	}
+
+}
---
+++ app_api_rest_authentification/src/test/java/com/app_api_rest_authentification/app_api_rest_authentification/AppApiRestAuthentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app_api_rest_authentification.app_api_rest_authentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApiRestAuthentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
