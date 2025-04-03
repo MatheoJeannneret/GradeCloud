cb2d7235caffb56f22abb06db024c2f3270c0a5f
JoachimDarazs <120582532+JoachimDarazs@users.noreply.github.com>
2025-04-03 16:38:26 +0200

    comit gestion notes 03.04.25

---
+++ app_api_rest_gestion_notes/src/test/java/com/app_api_rest_gestion_note/app_api_rest_gestion_notes/AppApiRestGestionNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app_api_rest_gestion_note.app_api_rest_gestion_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApiRestGestionNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
