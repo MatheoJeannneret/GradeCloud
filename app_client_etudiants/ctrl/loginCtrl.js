2caa04d36530c039010a03f194fa64a278e289d7
JoachimDarazs <120582532+JoachimDarazs@users.noreply.github.com>
2025-04-14 11:27:41 +0200

    comit eleve

--- app_client_etudiants/ctrl/loginCtrl.js
+++ app_client_etudiants/ctrl/loginCtrl.js
@@ -1,55 +1,18 @@
-function connectSuccess(data, text, jqXHR) {
-  if (jqXHR.status === 200) {
-    // Stocker l'état de connexion dans sessionStorage
-    sessionStorage.setItem("isConnected", "1");
-
-    alert("Vous êtes connectés avec succès !");
-    // Rediriger vers la page des produits
-    window.location.href = "../ihm/main.html";
-  }
-}
-
-function CallbackError(request, status, error) {
-  if (request.status === 401) {
-    alert("Le mot de passe est incorrect");
-  } else if (request.status === 404) {
-    alert("Cet utilisateur n'existe pas");
-  } else if (request.status === 400) {
-    alert("Merci de remplir tous les champs");
-  } else {
-    alert(
-      "erreur : " +
-        error +
-        ", request: " +
-        request.status +
-        ", status: " +
-        status
-    );
-  }
-}
-async function hashPassword(password) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(password);
-  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray
-    .map((b) => b.toString(16).padStart(2, "0"))
-    .join("");
-  return hashHex;
-}
-
 $(document).ready(function () {
-  var butConnect = $("#btnConnect");
-  butConnect.click(async function (event) {
-    event.preventDefault();
+    const loginCtrl = new LoginCtrl();
+    const serviceHttp = new ServiceHttp();
+    var butConnect = $("#btnConnect");
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    butConnect.click(async function (event) {
+        event.preventDefault();
 
-    const hashedPassword = await hashPassword(password); // ATTEND le hash correctement
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        sessionStorage.setItem("username", username);
+        const hashedPassword = await loginCtrl.hashPassword(password); // ATTEND le hash correctement
 
-    connect(username, hashedPassword, connectSuccess, CallbackError);
-  });
+        serviceHttp.connect(username, hashedPassword, loginCtrl.connectSuccess, loginCtrl.CallbackError);
+    });
 });
 
 class LoginCtrl {
